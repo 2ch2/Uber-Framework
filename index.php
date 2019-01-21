e166576209a84378538df5655dd26235790509cb
Kamil Kozaczyński <kamil.ubermade@gmail.com>
2019-01-21 23:25:25 +0100

    Create bootstrap and exception handler for routing

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
 <?php
 
-include __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/config/config.php';
-require_once __DIR__ . '/config/routes.php';
-
-$router = new uber\Core\Router\Router(SERVER_PROTOCOL . '://' . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']);
-
-$router->run();
-
-$file = $router->getFile();
-$class = $router->getClass();
-$method = $router->getMethod();
-
-if (file_exists($file)) {
-    require_once $file;
-    $obj = new $class();
-    $obj->$method();
-} else {
-    echo 'Page not found.';
-}
+require __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/bootstrap/app.php';
